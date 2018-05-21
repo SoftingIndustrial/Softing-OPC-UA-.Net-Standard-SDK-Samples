1a957fbdc69111254341f795d695f419fccbb14e
Alin Moldovean <aam@softing.com>
2018-05-21 06:57:26 +0000

    Save nodes to xml in ToolkitTestNodeManager

--- Server/SampleServerToolkit/DataAccess/ToolkitTestNodeManager.cs
+++ Server/SampleServerToolkit/DataAccess/ToolkitTestNodeManager.cs
@@ -8,14 +8,15 @@
  * 
  * ======================================================================*/
 
-using Opc.Ua;
-using Opc.Ua.Server;
-using Softing.Opc.Ua.Server;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Threading;
+using Opc.Ua;
+using Opc.Ua.Server;
+using Softing.Opc.Ua.Server;
 
 namespace SampleServerToolkit.ToolkitTest
 {
@@ -58,11 +59,10 @@ namespace SampleServerToolkit.ToolkitTest
                 // Create test variable nodes.
                 FolderState testVariables = CreateFolder(folder, "TestVariables");
 
-                PropertyState property = CreateProperty(testVariables, "Property", DataTypeIds.Int32, ValueRanks.Scalar);
+                PropertyState property = CreateProperty(testVariables, "Property", DataTypeIds.LocalizedText, ValueRanks.Scalar);
                 PropertyState<bool[]> typedProperty = CreateProperty<bool[]>(testVariables, "TypedProperty");
 
                 BaseDataVariableState<QualifiedName> typedVariable = CreateVariable<QualifiedName>(testVariables, "TypedVariable");
-
                 BaseDataVariableState byteVariable = CreateVariable(testVariables, "ByteVariable", DataTypeIds.Byte);
                 BaseDataVariableState stringVariable = CreateVariable(testVariables, "StringVariable", DataTypeIds.String, ValueRanks.Scalar);
                 BaseDataVariableState intArrayVariable = CreateVariable(testVariables, "Int32Array", DataTypeIds.Int32, ValueRanks.OneDimension);
@@ -145,6 +145,21 @@ namespace SampleServerToolkit.ToolkitTest
                 }
 
                 m_pollingTimer = new Timer(ReadDynamicValues, null, 1000, 1000);
+
+                // Save nodes to XML file
+                NodeStateCollection nodeCollection = new NodeStateCollection();
+
+                foreach (NodeState node in PredefinedNodes.Values)
+                {
+                    nodeCollection.Add(node);
+                }
+
+                if (nodeCollection != null)
+                {
+                    Stream file = new FileStream("Nodes.xml", FileMode.OpenOrCreate);
+
+                    nodeCollection.SaveAsNodeSet2(SystemContext, file);
+                }
             }
         }
 
