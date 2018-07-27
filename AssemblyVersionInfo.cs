de6785713610dd6e6173538f70f15c45eb26f8b6
Generation <generation@softing.com>
2018-07-27 10:53:53 +0000

    /* ========================================================================  * Copyright Â© 2011-2018 Softing Industrial Automation GmbH.  * All rights reserved.  *  * The Software is subject to the Softing Industrial Automation GmbHâ€™s  * license agreement, which can be found here:  * http://www.softing.com/LicenseSIA.pdf  *  * The Software is based on the OPC Foundation, Inc.â€™s software. This  * original OPC Foundationâ€™s software can be found here:  * http://www.opcfoundation.org  *  * The original OPC Foundationâ€™s software is subject to the OPC Foundation  * MIT License 1.00, which can be found here:  * http://opcfoundation.org/License/MIT/1.00/  *  * ======================================================================*/
    using System.Reflection;
    
    [assembly: AssemblyCopyright(AssemblyVersionInfo.Copyright)]
    [assembly: AssemblyVersion(AssemblyVersionInfo.CurrentVersion)]
    [assembly: AssemblyFileVersion(AssemblyVersionInfo.CurrentFileVersion)]
    [assembly: AssemblyCompany(AssemblyVersionInfo.AssemblyCompany)]
    [assembly: AssemblyProduct(AssemblyVersionInfo.AssemblyProduct)]
    [assembly: AssemblyTrademark("")]
    [assembly: AssemblyCulture("")]
    #if DEBUG
    [assembly: AssemblyConfiguration("Debug")]
    #else
    [assembly: AssemblyConfiguration("Release")]
    #endif
    
    /// <summary>
    /// Defines string constants for Toolkit version information.
    /// </summary>
    internal static class AssemblyVersionInfo
    {
        /// <summary> The current copy right notice. </summary>
        public const string Copyright = "Â© 2018 Softing Industrial Automation GmbH";
        
        /// BEWARE THAT IN VS 2017 THE USAGE OF * PLACEHOLDER IS NOT ALLOWED
        /// <summary> The current build version. </summary>
        public const string CurrentVersion = "2.0.0.804";
    
        /// <summary> The current build file version. </summary>
        public const string CurrentFileVersion = "2.0.0.804";
    
        /// <summary>The assembly copyright owner.</summary>
        public const string AssemblyCompany = "Softing Industrial Automation GmbH";
    
        /// <summary>The product name.</summary>
        public const string AssemblyProduct = "Softing OPC UA .NET Standard Toolkit";
    }

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -41,10 +41,10 @@ internal static class AssemblyVersionInfo
     
     /// BEWARE THAT IN VS 2017 THE USAGE OF * PLACEHOLDER IS NOT ALLOWED
     /// <summary> The current build version. </summary>
-    public const string CurrentVersion = "2.0.0.800";
+    public const string CurrentVersion = "2.0.0.804";
 
     /// <summary> The current build file version. </summary>
-    public const string CurrentFileVersion = "2.0.0.800";
+    public const string CurrentFileVersion = "2.0.0.804";
 
     /// <summary>The assembly copyright owner.</summary>
     public const string AssemblyCompany = "Softing Industrial Automation GmbH";
