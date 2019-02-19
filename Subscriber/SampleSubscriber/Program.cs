131b3f276c4034bc9f6bf9922c5e14b7b2eb2db5
Florin Neamtu <Florin.Neamtu@Softing.com>
2019-02-19 11:59:26 +0000

    Create a Softing.Opc.Ua.PubSub VS 2017 project Create PubSub sample project solution - with two projects: Publisher and Subscriber

---
+++ Subscriber/SampleSubscriber/Program.cs
@@ -0,0 +1,26 @@
+﻿/* ========================================================================
+ * Copyright © 2011-2019 Softing Industrial Automation GmbH. 
+ * All rights reserved.
+ * 
+ * The Software is subject to the Softing Industrial Automation GmbH’s 
+ * license agreement, which can be found here:
+ * http://www.softing.com/LicenseSIA.pdf
+ * 
+ * ======================================================================*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamplePublisher
+{
+    static class Program
+    {
+        /// <summary>
+        /// Entry point for application
+        /// </summary>
+        static void Main()
+        {
+        }
+    }
+}
