10a507f943b97b83eae3f9a879d8cafcdf9c06cb
Florin Neamtu <Florin.Neamtu@Softing.com>
2019-07-17 07:52:44 +0000

    Change license header in source code files

--- Client/SampleClient/StateMachine/Enums.cs
+++ Client/SampleClient/StateMachine/Enums.cs
@@ -4,7 +4,7 @@
  * 
  * The Software is subject to the Softing Industrial Automation GmbH’s 
  * license agreement, which can be found here:
- * http://www.softing.com/LicenseSIA.pdf
+ * https://data-intelligence.softing.com/LA-SDK-en/
  *  
  * ======================================================================*/
  
