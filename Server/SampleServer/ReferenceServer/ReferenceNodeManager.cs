ec7d4d0dfdecee26599ff0bd893c9400a959c606
Stan Octavia <Octavia-Stan.Buta@Softing.com>
2019-09-24 13:16:12 +0000

    fix qualified name array value

--- Server/SampleServer/ReferenceServer/ReferenceNodeManager.cs
+++ Server/SampleServer/ReferenceServer/ReferenceNodeManager.cs
@@ -525,7 +525,7 @@ namespace SampleServer.ReferenceServer
                     CreateAnalogVariable(analogArrayFolder,  "Number", BuiltInType.Number, ValueRanks.OneDimension,
                          initialValues: new Int16[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
                     CreateAnalogVariable(analogArrayFolder,  "QualifiedName", BuiltInType.QualifiedName, ValueRanks.OneDimension,
-                         initialValues: new Int16[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+                         initialValues: new QualifiedName[] { new QualifiedName("one"), new QualifiedName("two"), new QualifiedName("three") });
                     CreateAnalogVariable(analogArrayFolder,  "SByte", BuiltInType.SByte, ValueRanks.OneDimension,
                          initialValues: new SByte[] { 10, 20, 30, 40, 50, 60, 70, 80, 90 });
                     CreateAnalogVariable(analogArrayFolder,  "String", BuiltInType.String, ValueRanks.OneDimension,
