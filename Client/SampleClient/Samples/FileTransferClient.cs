a29d64d6b2edfb227c69cf7f6d49a8b059a8ba93
Florin Neamtu <Florin.Neamtu@Softing.com>
2021-10-22 11:30:51 +0000

    TT#2138 fixed: Client option: File Transfer menu options get null results

--- Client/SampleClient/Samples/FileTransferClient.cs
+++ Client/SampleClient/Samples/FileTransferClient.cs
@@ -24,16 +24,16 @@ namespace SampleClient.Samples
         #region Private Fields
 
         // the nodeId of the DownloadFile which is specified on the File Transfer Node Manager
-        private const string DownloadNodeID = "ns=9;i=22";
-        
+        private const string DownloadNodeID = "ns=9;i=2";
+
         // the nodeId of the UploadFile from the server
-        private const string UploadNodeID = "ns=9;i=63";
-        
+        private const string UploadNodeID = "ns=9;i=24";
+
         // the nodeId of the ByteString element from the server
-        private const string ByteStringNodeID = "ns=9;i=84";
+        private const string ByteStringNodeID = "ns=9;i=46";
 
         // the nodeId of the TemporaryFile element from the server
-        private const string TemporaryFileNodeID = "ns=9;i=95";
+        private const string TemporaryFileNodeID = "ns=9;i=47";
 
         private string DownloadFilePath = Path.Combine("Files", "DownloadFile.xml");
         private string UploadFilePath = Path.Combine("Files", "UploadClientFile.xml");
