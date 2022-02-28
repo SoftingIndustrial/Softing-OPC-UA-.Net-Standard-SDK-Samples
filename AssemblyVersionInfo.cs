93c43ac4cd3c36498c8081fd9233f1529e8c3b75
netsdt <NetStdSdk-generation@softing.com>
2022-02-28 07:23:07 +0000

    update version number

--- AssemblyVersionInfo.cs
+++ AssemblyVersionInfo.cs
@@ -41,10 +41,10 @@ internal static class AssemblyVersionInfo
 
     /// BEWARE THAT IN VS 2017 THE USAGE OF * PLACEHOLDER IS NOT ALLOWED
     /// <summary> The current build version. </summary>
-    public const string CurrentVersion = "3.10.0.3319";
+    public const string CurrentVersion = "3.10.0.3327";
 
     /// <summary> The current build file version. </summary>
-    public const string CurrentFileVersion = "3.10.0.3319";
+    public const string CurrentFileVersion = "3.10.0.3327";
 
     /// <summary>The assembly copyright owner.</summary>
     public const string AssemblyCompany = "Softing Industrial Automation GmbH";
@@ -53,5 +53,5 @@ internal static class AssemblyVersionInfo
     public const string AssemblyProduct = "Softing OPC UA .NET Standard SDK";
 
 	/// <summary>The product release date.</summary>
-    public const string ReleaseDate = "2022/02/25 15:12:19";
+    public const string ReleaseDate = "2022/02/28 09:23:52";
 }
